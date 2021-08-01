12ed95db258bc9d5147e7b99e1161cb6f9beeb72
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2021-08-01 16:22:51 +0000

    Add drawSnake and moveSnake functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,58 @@
 let canvas = document.getElementById('game-area');
-let ctx = getContext('2d');
-let snakeSpeed = 7
+let ctx = canvas.getContext('2d');
+let snakeSpeed = 5;
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+let headX = 10;
+let headY = 10;
+let xVelocity = 0;
+let yVelocity = 0;
 
 function makeGame() {
-    console.log('draw game');
-    setTimeout(makeGame, 1000/ speed); 
+    clearBoard();
+    moveSnake();
+    drawSnake();
+    setTimeout(makeGame, 1000/ snakeSpeed); 
+}
+
+function clearBoard() {
+    ctx.fillStyle = '#22718F';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawSnake() {
+    ctx.fillStyle = '#7AAFC6';
+    ctx.fillRect(headX * tileCount + 1, headY * tileCount + 1, tileSize, tileSize);
+}
+
+function moveSnake() {
+    headX = headX + xVelocity;
+    headY = headY + yVelocity;
+}
+
+document.body.addEventListener('keydown', keyDown);
+
+function keyDown(event) {
+    //up
+    if(event.keyCode == 38) {
+        yVelocity = -1;
+        xVelocity = 0;
+    }
+    //right
+    if(event.keyCode == 39) {
+        yVelocity = 0;
+        xVelocity = 1;
+    }
+    //left
+    if(event.keyCode == 37) {
+        yVelocity = 0;
+        xVelocity = -1;
+    }
+    //down
+    if(event.keyCode == 40) {
+        yVelocity = 1;
+        xVelocity = 0;
+    }
 }
 
 makeGame();
